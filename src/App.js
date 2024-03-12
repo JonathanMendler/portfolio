d7edf1d7cfd860df72ca7ab7643bd8d9420c07ae
Jonathan Mendler <jon.mendler@gmail.com>
2024-03-12 09:56:29 -0500

    Finished Projects 1st draft

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Header from "./components/Header";
 import Champion from "./components/Champion";
 import About from "./components/About";
+import Projects from "./components/Projects";
 import Footer from "./components/Footer";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Champion />
       <About />
+      <Projects />
       <Footer />
     </>
   );
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,6 @@
 import React from "react";
-import CoCa from "../assets/coca.png";
-import DevLoom from "../assets/devloom.png";
+import CoCa from "../assets/CoCa.png";
+import DevLoom from "../assets/placeholder.png";
 
 const Projects = () => {
   const projects = [
@@ -19,4 +19,45 @@ const Projects = () => {
       code: "https://github.com/JonathanMendler/DevLoom",
     },
   ];
+
+  return (
+    <section className="bg-primary text-white px-5 py-32" id="projects">
+      <div className="container mx-auto grid md:grid-cols-2 items-center md:justify-between">
+        <div className="about-info mb-5">
+          <h2 className="text-4xl font-bold mb-5 border-b-[5-px] w-[180px] border-indigo-600 pb-2">Projects</h2>
+
+          <p className="pb-5">
+            Have a look at some of my best projects! These were built utilizing Ruby on Rails, JavaScript, CSS3, React,
+            Bootstrap, and TailwindCSS.
+          </p>
+        </div>
+
+        <div className="about-img"></div>
+      </div>
+
+      <div className="projects container mx-auto grid md:grid-cols-2 gap 10">
+        {projects.map((project, i) => {
+          return (
+            <div className="relative" key={i}>
+              <img src={project.img} alt={project.title} />
+              <div className="flex absolute left-0 right-0 top-[13px] bottom-0 mx-auto w-[90%] h-[90%] bg-primary opacity-0 duration-500 justify-center flex-col hover:opacity-100 ">
+                <p className="py-5 text-center font-bold px-2 text-white">{project.desc}</p>
+
+                <div className="mx-auto">
+                  <a href={project.live} className="px-5 py-2 bg-blue-500 hover:bg-blue-600 mr-5 font-bold">
+                    Live
+                  </a>
+                  <a href={project.code} className="px-5 py-2 bg-blue-700 hover:bg-blue-800 font-bold">
+                    Code
+                  </a>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
+
+export default Projects;
