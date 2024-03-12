05be9e3d1762d92c55eabac568e68d7a83353329
Jonathan Mendler <jon.mendler@gmail.com>
2024-03-12 10:07:43 -0500

    Finished Contact 1st draft

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import Champion from "./components/Champion";
 import About from "./components/About";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Champion />
       <About />
       <Projects />
+      <Contact />
       <Footer />
     </>
   );
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,6 +2,23 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <section className="bg-secondary px-5 py-32"
-  )
-}
+    <section className="bg-secondary px-5 py-32" id="contact">
+      <div className="text-center md:w-[60%] mx-auto text-white">
+        <h2 className="text-4xl font-bold mb-5 border-b-[5px] w-[200px] mx-auto border-indigo-600 pb-2">Contact Me</h2>
+        <p>
+          I am currently seeking a Full Stack Developer role. If you would like to discuss any open positions with me,
+          you can contact me via one of the following methods.
+        </p>
+
+        <p className="py-2">
+          <span className="font-bold">Email:</span> Jon.mendler@gmail.com
+        </p>
+        <p className="py-2">
+          <span className="font-bold">Phone:</span> +1 (219)669-3013
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
