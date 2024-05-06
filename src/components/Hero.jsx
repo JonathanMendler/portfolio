a2007b5083899d8600f906768149ec8eaa6db1af
Jonathan Mendler <jon.mendler@gmail.com>
2024-05-06 15:24:25 -0500

    Update index.css

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ import { RiGithubFill, RiLinkedinBoxFill } from "react-icons/ri";
 
 const Hero = () => {
   return (
-    <section className="bg-primary px-5 text-white py-32">
+    <section className="bg-secondary px-5 text-white py-32">
       <div className="container mx-auto grid md:grid-cols-2 items-center justify-center md:justify-between">
         <div className="hero-info pb-5 md:pb-0">
           <h1 className="text-4xl lg:text-6xl">
@@ -16,7 +16,7 @@ const Hero = () => {
 
           <p className="py-5">I am proficient in Ruby on Rails, JavaScript, React.js, Vite and Tailwind CSS</p>
 
-          <div className="flex py-5 ">
+          <div className="flex py-5">
             <a href="https://github.com/JonathanMendler" className="pr-4 inline-block text-accent hover:text-white">
               {" "}
               <RiGithubFill size={40} />{" "}
@@ -32,7 +32,7 @@ const Hero = () => {
 
           <a
             href="/#projects"
-            className=" btn bg-accent  border-2 border-[#7477FF] text-white px-6 py-3 rounded-3xl hover:bg-transparent"
+            className=" btn bg-accent border-2 border-[#7477FF] text-white px-6 py-3 rounded-3xl hover:bg-transparent"
           >
             See Projects
           </a>
