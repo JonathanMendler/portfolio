23d37bbde6674a22e2d11a3ae2e4b8cbb79220a2
Jonathan Mendler <jon.mendler@gmail.com>
2024-05-07 10:43:20 -0500

    Add Footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
-// import Footer from "./components/Footer";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -14,7 +14,7 @@ function App() {
       <About />
       <Projects />
       <Contact />
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="py-4 text-center bg-secondary text-white ">
+      {" "}
+      &copy; 2024 | Jonathan Mendler | All Right Reserved
+    </div>
+  );
+};
+
+export default Footer;
