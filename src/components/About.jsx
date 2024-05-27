15126655cc55b3db1644b67ba1651bf85e9708c5
Jonathan Mendler <jon.mendler@gmail.com>
2024-05-27 18:36:03 -0500

    new color | About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,10 +3,10 @@ import AboutImg from "../assets/tech.svg";
 
 const About = () => {
   return (
-    <section className="bg-primary text-white px-5 py-32" id="about">
+    <section className="bg-primary text-teal-800 px-5 py-32" id="about">
       <div className="container mx-auto grid md:grid-cols-2 items-center justify-center md:justify-between">
         <div className="about-info">
-          <h2 className="text-4xl font-bold mb-5 border-b-[5px] w-[180px] border-indigo-600 pb-2">About Me</h2>
+          <h2 className="text-4xl font-bold mb-5 border-b-[5px] w-[180px] border-yellow-400 pb-2">About Me</h2>
 
           <p className="pb-5">
             My journey in the realm of software engineering began with a deep fascination for problem-solving and a
