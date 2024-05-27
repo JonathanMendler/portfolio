acacfd658a73e35a5775d41d1dfeffca474f901f
Jonathan Mendler <jon.mendler@gmail.com>
2024-05-27 18:49:45 -0500

    new color | Contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,8 +4,8 @@ import { RiDownload2Fill } from "react-icons/ri";
 const Contact = () => {
   return (
     <section className="bg-primary px-5 py-32" id="contact">
-      <div className="text-center md:w-[60%] mx-auto text-white">
-        <h2 className="text-4xl font-bold mb-5 border-b-[5px] w-[200px] mx-auto border-indigo-600 pb-2">Contact Me</h2>
+      <div className="text-center md:w-[60%] mx-auto text-teal-800">
+        <h2 className="text-4xl font-bold mb-5 border-b-[5px] w-[200px] mx-auto border-yellow-400 pb-2">Contact Me</h2>
         <p>Interested in building something together? Let's chat!</p>
 
         <p className="mt-3 py-1">
@@ -20,7 +20,7 @@ const Contact = () => {
         </p>
         <button
           type="button"
-          className="inline-flex items-center gap-x-1.5 rounded-md bg-indigo-600 mt-3 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          className="inline-flex items-center gap-x-1.5 rounded-md bg-teal-800 mt-3 px-3 py-2 text-sm font-semibold text-yellow-400 shadow-sm hover:bg-teal-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-800"
         >
           <RiDownload2Fill className="-ml-0.5 h-5 w-5" aria-hidden="true" />
           Resume
